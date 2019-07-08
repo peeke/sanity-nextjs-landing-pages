e8740ed0abaffc2bf6149b47a45340fc27710d10
Peeke <pkuepers@kaliber.net>
2019-07-08 16:39:42 +0200

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-nextjs-landing-pages'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5d2355abc09ad5a3fdaed254',
+                  title: 'Sanity Studio',
+                  name: 'sanity-nextjs-landing-pages-studio-43ow9uoz',
+                  apiId: 'b237a9e2-5a73-4ce3-b2bc-bbdde91391ea'
+                },
+                {
+                  buildHookId: '5d2355abb8ccf2edbc6cedec',
+                  title: 'Landing pages Website',
+                  name: 'sanity-nextjs-landing-pages-web-yrqnwtbp',
+                  apiId: '868e465c-117f-4aa0-ae0c-8ffab83c80b6'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/peeke/sanity-nextjs-landing-pages',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://sanity-nextjs-landing-pages-web-yrqnwtbp.netlify.com', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: ['page']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
